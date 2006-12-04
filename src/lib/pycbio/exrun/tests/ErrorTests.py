bbcd999263c1a792e810a82ee59296b8ffeec602
markd <none@none>
2006-12-04 19:29:19 +0000

    work on exrun

--- src/lib/pycbio/exrun/tests/ErrorTests.py
+++ src/lib/pycbio/exrun/tests/ErrorTests.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     sys.path.append("../../..")
 from pycbio.sys.TestCaseBase import TestCaseBase
 from pycbio.exrun.ExRun import ExRun,ExRunException
-from pycbio.exrun.Graph import Rule,CycleException
+from pycbio.exrun.Graph import Rule,GraphException, CycleException
 
 # FIXME add:
 #  dup production test
@@ -22,57 +22,58 @@ class GraphTests(TestCaseBase):
     def testCycleAll(self):
         "all nodes in a cycle (no entry)"
         id = self.getId()
-        er = ExRun()
-        # use id so file path doesn't vary with run directory
-        f1 = er.getFile(id + ".file1")
-        f2 = er.getFile(id + ".file2")
-        f3 = er.getFile(id + ".file3")
-        er.addRule(ErrorRule("rule1", f1, f2))
-        er.addRule(ErrorRule("rule2", f2, f3))
-        er.addRule(ErrorRule("rule3", f3, f1))
         ex = None
         try:
+            er = ExRun()
+            # use id so file path doesn't vary with run directory
+            f1 = er.getFile(id + ".file1")
+            f2 = er.getFile(id + ".file2")
+            f3 = er.getFile(id + ".file3")
+            er.addRule(ErrorRule("rule1", f1, f2))
+            er.addRule(ErrorRule("rule2", f2, f3))
+            er.addRule(ErrorRule("rule3", f3, f1))
             er.run()
         except CycleException, ex:
-            self.failUnlessEqual(str(ex), "cycle detected:\n  rule1 ->\n  ErrorTests.GraphTests.testCycleAll.file2 ->\n  rule2 ->\n  ErrorTests.GraphTests.testCycleAll.file3 ->\n  rule3 ->\n  ErrorTests.GraphTests.testCycleAll.file1 ->")
+            self.failUnlessEqual(str(ex), "cycle detected:\n  rule3 ->\n  ErrorTests.GraphTests.testCycleAll.file3 ->\n  rule2 ->\n  ErrorTests.GraphTests.testCycleAll.file2 ->\n  rule1 ->\n  ErrorTests.GraphTests.testCycleAll.file1 ->")
         if ex == None:
             self.fail("expected CycleException")
         
     def testCycle(self):
         "entry node and cycle"
+        # can't build this graph due to linking sanity checks            
         id = self.getId()
-        er = ExRun()
-        # use id so file path doesn't vary with run directory
-        f1 = er.getFile(id + ".file1")
-        f2 = er.getFile(id + ".file2")
-        f3 = er.getFile(id + ".file3")
-        er.addRule(ErrorRule("rule1", f1, f2))
-        er.addRule(ErrorRule("rule2", f2, f3))
-        er.addRule(ErrorRule("rule3", f3, f2))
         ex = None
         try:
+            er = ExRun()
+            # use id so file path doesn't vary with run directory
+            f1 = er.getFile(id + ".file1")
+            f2 = er.getFile(id + ".file2")
+            f3 = er.getFile(id + ".file3")
+            er.addRule(ErrorRule("rule1", f1, f2))
+            er.addRule(ErrorRule("rule2", f2, f3))
+            er.addRule(ErrorRule("rule3", f3, f2))
             er.run()
-        except CycleException, ex:
-            self.failUnlessEqual(str(ex),"cycle detected:\n  rule2 ->\n  ErrorTests.GraphTests.testCycle.file3 ->\n  rule3 ->\n  ErrorTests.GraphTests.testCycle.file2 ->")
+        except GraphException, ex:
+            self.failUnlessEqual(str(ex), "Production ErrorTests.GraphTests.testCycle.file2 already linked to it's produces, attempt to add: rule3")
         if ex == None:
-            self.fail("expected CycleException")
+            self.fail("expected GraphException")
 
     def testNoRule(self):
         "no rule to make a production"
 
         id = self.getId()
-        er = ExRun()
-        # use id so file path doesn't vary with run directory
-        f1 = er.getFile(id + ".file1")
-        f2 = er.getFile(id + ".file2")
-        f3 = er.getFile(id + ".file3")
-        er.addRule(ErrorRule("rule1", f3, f2))
-        er.addRule(ErrorRule("rule2", f2, f1))
         ex = None
         try:
+            er = ExRun()
+            # use id so file path doesn't vary with run directory
+            f1 = er.getFile(id + ".file1")
+            f2 = er.getFile(id + ".file2")
+            f3 = er.getFile(id + ".file3")
+            er.addRule(ErrorRule("rule1", f3, f2))
+            er.addRule(ErrorRule("rule2", f2, f1))
             er.run()
         except ExRunException, ex:
-            self.failUnlessEqual(str(ex),"no rule to build: ErrorTests.GraphTests.testNoRule.file1")
+            self.failUnlessEqual(str(ex),"no rule to build: ErrorTests.GraphTests.testNoRule.file3")
         if ex == None:
             self.fail("expected ExRunException")
         
