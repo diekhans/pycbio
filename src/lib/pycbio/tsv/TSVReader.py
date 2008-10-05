541ac60d181fdc0b2acff11da37ba2b6d8820ec5
markd <none@none>
2008-10-05 14:05:18 +0000

    use csv module for tab files

--- src/lib/pycbio/tsv/TSVReader.py
+++ src/lib/pycbio/tsv/TSVReader.py
@@ -1,10 +1,8 @@
 """TSV reading classes"""
-import sys
+import sys,csv
 from pycbio.sys import fileOps
 from pycbio.tsv.TSVRow import TSVRow
 from pycbio.tsv.TSVError import TSVError
-
-# FIXME: base on new std python cvs module
 
 # FIXME:  pass owndership of row to Row instead of having Row inherit from list
 # FIXME: create error with file name/line number
@@ -24,19 +22,30 @@ class TSVReader(object):
     becomes a field name.  It is also indexable by column name or int index.
     Columns can be automatically type converted by column name."""
 
-    def _readHeader(self):
-        line = self.inFh.readline()
-        line = line[0:-1]  # do first to catch blank line
-        if (line == ""):
-            raise TSVError("empty TSV file or header", reader=self),sys.exc_info()[2]
-        if line[0] == '#':
-            line = line[1:]
-        if self.isRdb:
-            self.inFh.readline() # skip format line
-        columns = line.split("\t")
-        self._setupColumns(columns)
+    def __readRow(self):
+        "read the next row, returning None on EOF"
+        try:
+            row = self.csvRdr.next()
+        except Exception, e:
+            self.close()
+            if isinstance(e, StopIteration):
+                return None
+            else:
+                raise
+        self.lineNum = self.csvRdr.line_num
+        return row
 
-    def _setupColumns(self, columns):
+    def __readHeader(self):
+        row = self.__readRow()
+        if row == None:
+            raise TSVError("empty TSV file", reader=self), sys.exc_info()[2]
+        if self.isRdb:
+            self.__readRow() # skip format line
+        if (len(row) > 0) and row[0].startswith('#'):
+            row[0] = row[0][1:]
+        self.__setupColumns(row)
+
+    def __setupColumns(self, columns):
         # n.b. columns could be passed in from client, must copy
         self.columns = []
         self.colMap = {}
@@ -47,7 +56,7 @@ class TSVReader(object):
             self.colMap[col] = i
             i += 1
 
-    def _initColTypes(self, typeMap, defaultColType):
+    def __initColTypes(self, typeMap, defaultColType):
         "save col types as column indexed list"
         if typeMap != None:
             # build from type map
@@ -89,13 +98,14 @@ class TSVReader(object):
         else:
             self.inFh = fileOps.opengz(fileName, "r")
         try:
+            self.csvRdr = csv.reader(self.inFh, dialect=csv.excel_tab)
             if columns:
-                self._setupColumns(columns)
+                self.__setupColumns(columns)
             else:
-                self._readHeader()
-            self._initColTypes(typeMap, defaultColType)
+                self.__readHeader()
+            self.__initColTypes(typeMap, defaultColType)
         except Exception, e:
-            self.inFh.close()
+            self.close()
             raise
 
     def close(self):
@@ -103,19 +113,16 @@ class TSVReader(object):
         by iter on EOF."""
         if self.inFh != None:
             self.inFh.close()
-        self.inFh = None
+            self.inFh = None
+            self.csvRdr = None
 
     def __iter__(self):
         return self
 
     def next(self):
-        self.lineNum += 1
-        line = self.inFh.readline()
-        if (line == ""):
-            self.close();
+        row = self.__readRow()
+        if row == None:
             raise StopIteration
-        line = line[0:-1]  # drop newline
-        row = line.split("\t")
         if ((self.ignoreExtraCols and (len(row) < len(self.columns)))
             or ((not self.ignoreExtraCols) and (len(row) != len(self.columns)))):
             # FIXME: will hang: self.close()
--- src/lib/pycbio/tsv/TabFile.py
+++ src/lib/pycbio/tsv/TabFile.py
@@ -1,25 +1,14 @@
 
 ## FIXME: needed for faster readings, but needs cleaned up, need reader/writer
 ## classes
+# FIXME add try and error msg with file/line num, move to row reader class; see fileOps.iterRows
 from pycbio.sys import fileOps
+import csv
 
 class TabFile(list):
     """Class for reading tab-separated files.  Can be used standalone
        or read into rows of a specific type.
     """
-
-   # FIXME add try and error msg with file/line num, move to row reader class; see fileOps.iterRows
-    def _read(self, inFh):
-        lineNum = 0
-        for line in inFh:
-            if not (self.hashAreComments and line.startswith('#')):
-                    line=line[0:-1]
-                    row = line.split("\t")
-                    if self.rowClass:
-                        self.append(self.rowClass(row))
-                    else:
-                        self.append(row)
-                    lineNum += 1
 
     def __init__(self, fileName, rowClass=None, hashAreComments=False):
         """Read tab file into the object
@@ -46,37 +35,38 @@ class TabFile(list):
 
 class TabFileReader(object):
     def __init__(self, tabFile, hashAreComments=False):
-        self.fh = fileOps.opengz(tabFile)
+        self.inFh = fileOps.opengz(tabFile)
+        self.csvRdr = csv.reader(self.inFh, dialect=csv.excel_tab)
         self.hashAreComments = hashAreComments
         self.lineNum = 0
 
-    def _readLine(self):
+    def __readRow(self):
+        "read the next row, returning None on EOF"
         try:
-            self.lineNum += 1
-            line = self.fh.readline()
-            if (line == ""):
+            row = self.csvRdr.next()
+        except Exception, e:
+            self.close()
+            if isinstance(e, StopIteration):
                 return None
             else:
-                line = line[0:-1]  # drop newline
-                return line
-        except Exception, e:
-            # FIXME: loses traceback, need to close
-            raise ValueError(self.fh.name + ":" + str(self.lineNum) + ": " + str(e), e)
+                raise
+        self.lineNum = self.csvRdr.line_num
+        return row
 
     def __iter__(self):
         return self
 
     def next(self):
         while True:
-            line = self._readLine()
-            if line == None:
-                self.fh.close();
+            row = self.__readRow()
+            if row == None:
                 raise StopIteration
-            if not (self.hashAreComments and line.startswith("#")):
-                row = line.split("\t")
+            if not (self.hashAreComments and (len(row) > 0) and row[0].startswith("#")):
                 return row
 
     def close(self):
-        "early close"
-        self.fh.close()
-        self.fh = None
+        "close file, called automatically on EOF"
+        if self.inFh != None:
+            self.inFh.close()
+            self.inFh = None
+            self.csvRdr = None
