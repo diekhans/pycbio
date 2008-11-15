6ee33ddb030a562cc06d24cea22a5e21f6cb044f
markd <none@none>
2008-11-15 22:09:34 +0000

    fixes and enhancements

--- src/lib/pycbio/hgdata/GeneCheckDetails.py
+++ src/lib/pycbio/hgdata/GeneCheckDetails.py
@@ -1,26 +1,38 @@
-from pycbio.tsv.TSVTable import TSVTable
 from pycbio.tsv.TSVRow import TSVRow
-from pycbio.hgdata.AutoSql import strArrayType
+from pycbio.tsv.TSVTable import TSVTable
+from pycbio.tsv.TSVReader import TSVReader
 from pycbio.sys.Enumeration import Enumeration
 
-class GeneCheckDetails(TSVRow):
-    """Object wrapper for one record from the gene-check program details output """
-
-def parseFeature(val):
-    "feature is an int or blank"
-    if isinstance(val, str):
-        return -1
+def strOrNone(val):
+    "return None if val is zero length otherwise val"
+    if len(val) == 0:
+        return None
     else:
-        return int(val)
+        return intern(val)
 
-typeMap = {"feature": parseFeature,
-           "problem": intern}
+#acc	problem	info	chr	chrStart	chrEnd
+typeMap = {"acc": intern,
+           "problem": intern,
+           "info": strOrNone,
+           "chr": intern,
+           "chrStart": int,
+           "chrEnd": int}
 
+def cmpByLocation(gcd1, gcd2):
+    d = cmp(gcd1.chr, gcd2.chr)
+    if d == 0:
+        d = cmp(gcd1.chrStart, gcd2.chrStart)
+        if d == 0:
+            d = cmp(gcd1.chrEnd, gcd2.chrEnd)
+    return d
+
+class GeneCheckDetailsReader(TSVReader):
+    def __init__(self, fileName, isRdb=False):
+        TSVReader.__init__(self, fileName, typeMap=typeMap, isRdb=isRdb)
 
 class GeneCheckDetailsTbl(TSVTable):
     """Table of GeneCheckDetails objects loaded from a TSV.    """
     
-    def __init__(self, fileName):
-        TSVTable.__init__(self, fileName, rowClass=GeneCheckDetails,
-                          typeMap=typeMap, multiKeyCols="id")
+    def __init__(self, fileName, isRdb=False):
+        TSVTable.__init__(self, fileName, typeMap=typeMap, isRdb=isRdb)
         
