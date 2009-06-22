7d54d85bfd41837e5987037c9d239055c0fafa0d
markd <none@none>
2009-06-22 01:41:04 +0000

    use private field naming convention

--- src/lib/pycbio/sys/Immutable.py
+++ src/lib/pycbio/sys/Immutable.py
@@ -5,25 +5,26 @@ Base class used to define immutable objects
 class Immutable(object):
     """Base class to make an object instance immutable.  Call 
     Immutable.__init__(self) after construction to make immutable"""
-    
-    __slots__ = ("__immutable__",)
+
+    __immAttr = "_Immutable__immutable"
+    __slots__ = (__immAttr,)
 
     def __new__(cls, *args, **kvs):
         self = object.__new__(cls)
-        object.__setattr__(self, "__immutable__", False)
+        object.__setattr__(self, Immutable.__immAttr, False)
         return self
 
     def __init__(self):
         "makes derived objects immutable"
-        object.__setattr__(self, "__immutable__", True)
+        object.__setattr__(self, Immutable.__immAttr, True)
 
     def __setattr__(self, attr, value):
-        if self.__immutable__:
+        if self.__immutable:
             raise TypeError("immutable object", self)
         object.__setattr__(self, attr, value)
 
     def __delattr__(self, attr):
-        if self.__immutable__:
+        if self.__immutable:
             raise TypeError("immutable object", self)
         object.__delattr__(self, attr)
 
