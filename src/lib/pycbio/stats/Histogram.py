de37c55e2803dbd7a78c251cdb00c5e45dfa15eb
markd <devnull@localhost>
2009-12-15 17:40:26 +0000

    ease of use

--- src/lib/pycbio/stats/Histogram.py
+++ src/lib/pycbio/stats/Histogram.py
@@ -31,9 +31,10 @@ class NumData(list):
 
     def compute(self):
         "compute data range and total"
-        self.min = min(self)
-        self.max = max(self)
-        self.total = sum(self)
+        if len(self) > 0:
+            self.min = min(self)
+            self.max = max(self)
+            self.total = sum(self)
 
 class NumCntData(list):
     "data consisting of tuples of numbers and counts"
@@ -87,7 +88,7 @@ class Histogram(object):
         self.numBinsUse = None
         self.binFloorUse = None  # min to actually use for indexing
         self.binCeilUse = None   # max to actually use for indexing
-
+        self.bins = None         # computed by build()
         if (data != None):
             self.data.extend(data)
 
@@ -127,7 +128,10 @@ class Histogram(object):
         if (self.numBinsUse == None) and (self.binSizeUse == None):
             # default num bins and compute bin size from it below
             self.numBinsUse = 10
-        if self.binSizeUse == None:
+
+        if self.binMinUse == None:
+            self.binSizeUse = self.binFloorUse = self.binCeilUse = 0
+        elif self.binSizeUse == None:
             # compute bin size from num bins
             estBinSize = float(self.binMaxUse-self.binMinUse)/float(self.numBinsUse-1)
             self.binSizeUse = float(self.binMaxUse-self.binMinUse+estBinSize)/float(self.numBinsUse)
@@ -140,7 +144,7 @@ class Histogram(object):
             self.binFloorUse = self.binMinUse
             self.binCeilUse = self.binMaxUse
 
-    def __getBin(self, val):
+    def __getBinIdx(self, val):
         "Get the integer bin number for a value, or None to ignore"
         if self.binSizeUse == 0.0:
             return 0
@@ -152,41 +156,44 @@ class Histogram(object):
             return int((val-self.binFloorUse)/float(self.binSizeUse))
 
     def __mkBins(self):
-        histo = []
+        self.bins = []
         for i in xrange(self.numBinsUse):
-            histo.append(Bin(self, i, self.binFloorUse+(i*self.binSizeUse), self.binSizeUse))
-        return histo
+            self.bins.append(Bin(self, i, self.binFloorUse+(i*self.binSizeUse), self.binSizeUse))
 
-    def __binTupleData(self, histo):
+    def __binTupleData(self):
         for item in self.data:
-            iBin = self.__getBin(item[0])
+            iBin = self.__getBinIdx(item[0])
             if iBin != None:
-                histo[iBin].cnt += item[1]
+                self.bins[iBin].cnt += item[1]
 
-    def __binScalarData(self, histo):
+    def __binScalarData(self):
         for item in self.data:
-            iBin = self.__getBin(item)
+            iBin = self.__getBinIdx(item)
             if iBin != None:
-                histo[iBin].cnt += 1
+                self.bins[iBin].cnt += 1
 
-    def __computeFreqs(self, histo):
-        for bin in histo:
-            bin.freq = bin.cnt / float(len(self.data))
+    def __computeFreqs(self):
+        ndata = float(len(self.data))
+        for bin in self.bins:
+            if ndata != 0.0:
+                bin.freq = bin.cnt / ndata
+            else:
+                bin.freq = 0.0
                 
     def build(self):
-        "construct histogram from data, return list of bins"
+        "construct histogram from data, return list of bins (also in bins field)"
         self.__calcParams()
-        histo = self.__mkBins()
+        self.__mkBins()
         if self.isTupleData:
-            self.__binTupleData(histo)
+            self.__binTupleData()
         else:
-            self.__binScalarData(histo)
+            self.__binScalarData()
 
         # compute frequencies
         if self.data.total != 0.0:
-            self.__computeFreqs(histo)
+            self.__computeFreqs()
 
-        return histo
+        return self.bins
 
     def dump(self, fh, desc=None):
         self.__calcParams()
