4021b36c8e562c40195ac217af737e355dd6d97c
markd <devnull@localhost>
2010-03-16 15:52:47 +0000

    added converters that treat None as empty

--- src/lib/pycbio/tsv/__init__.py
+++ src/lib/pycbio/tsv/__init__.py
@@ -1,5 +1,5 @@
 from pycbio.tsv.TSVRow import TSVRow
-from pycbio.tsv.TSVReader import TSVReader
+from pycbio.tsv.TSVReader import TSVReader, strOrNoneType, intOrNoneType
 from pycbio.tsv.TSVTable import TSVTable
 from pycbio.tsv.TSVError import TSVError
 from pycbio.tsv.TabFile import TabFile
