515ac16baa6ba1e2bf7c5382dd754bf85305f407
markd <devnull@localhost>
2010-03-25 03:03:26 +0000

    added stuff for connecting to database

---
+++ src/lib/pycbio/hgdata/HgDb.py
@@ -0,0 +1,9 @@
+"""Connect to UCSC genome database using info in .hg.conf """
+from pycbio.hgdata import HgConf
+import MySQLdb
+
+def connect(db=None,  confFile=None):
+    """connect to genome mysql server, using confFile or ~/.hg.conf"""
+    conf = HgConf.obtain(confFile)
+    return MySQLdb.Connect(host=conf["db.host"], user=conf["db.user"], passwd=conf["db.password"], db=db)
+    
