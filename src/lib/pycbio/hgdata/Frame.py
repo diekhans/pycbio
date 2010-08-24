9c5e8a4419256ce165edf2394a27045d662958e6
markd <devnull@localhost>
2010-08-24 19:01:49 +0000

    cleanup

--- src/lib/pycbio/hgdata/Frame.py
+++ src/lib/pycbio/hgdata/Frame.py
@@ -4,7 +4,7 @@ def frameIncr(frame, amt):
     """increment an interger frame by positive or negative amount. Frame of -1
     already returns -1."""
     if frame < 0:
-        return frame;  # no frame not changed
+        return frame  # no frame not changed
     elif amt >= 0:
         return ((frame + amt) % 3)
     else:
@@ -38,10 +38,10 @@ class Frame(Immutable):
         """increment frame by positive or negative amount, returning a new
         Frame object.  Frame of -1 already returns -1."""
         if self.val < 0:
-            return self;  # no frame not changed
+            return self  # no frame, not changed
         elif amt >= 0:
             return Frame((self.val + amt) % 3)
         else:
-            amt3 = ((-amt)%3)
-            return Frame((self.val - (amt-amt3)) % 3)
+            amt3 = (-amt) % 3
+            return Frame((self.val - (amt - amt3)) % 3)
         
