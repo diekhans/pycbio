c0dd78c6f635f975af67898619ba6dfa76cd43c4
markd <none@none>
2010-11-30 08:43:02 +0000

    refactory chrom info classes

--- src/lib/pycbio/hgdata/ChromInfo.py
+++ src/lib/pycbio/hgdata/ChromInfo.py
@@ -1,5 +1,10 @@
+"""
+Object with chromosome information that can be loaded from a variety of
+sources.
+"""
 # Copyright 2006-2010 Mark Diekhans
 from pycbio.sys.procOps import callProc
+from pycbio.tsv import TabFile
 
 class ChromInfo(object):
     "object with chromosome information"
@@ -10,23 +15,12 @@ class ChromInfo(object):
 class ChromInfoTbl(dict):
     "object to manage information about chromosomes"
     
-    def __init__(self, db=None, nibDir=None):
-        if (db == None) and (nibDir == None):
-            raise Exception("must specify either db= or nibDir= arguments")
-        self.db = db
-        self.nibDir = nibDir
-        if self.nibDir == None:
-            self.nibDir = callProc(("genomeFind", db))
+    def __init__(self, chromClass=ChromInfo):
+        self.chromClass = chromClass
 
-    def _loadChrom(self, chrom):
-        "lazy loading of chromosome info"
-        nib = self.nibDir + "/" + chrom + ".nib"
-        line = callProc(["nibSize", nib])
-        cols = line.split("\t")
-        self[chrom] = ChromInfo(chrom, int(cols[2]))
-        
-    def __getitem__(self, chrom):
-        "get the ChromInfo object for a chrom"
-        if not chrom in self:
-            self._loadChrom(chrom)
-        return self.get(chrom)
+    def loadChromSizes(self, chromSizes):
+        "Load from chrom.sizes file"
+        for row in TabFile(chromSizes):
+            cs = self.chromClass(row[0], int(row[1]))
+            self[cs.chrom] = cs
+
