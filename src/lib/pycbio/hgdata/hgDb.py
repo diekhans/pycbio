9a59b32ae3fd979430528e734f70fcfb589a43b9
Mark Diekhans <markd@soe.ucsc.edu>
2014-12-21 11:32:17 -0800

    allow specifying a different host

--- src/lib/pycbio/hgdata/hgDb.py
+++ src/lib/pycbio/hgdata/hgDb.py
@@ -7,9 +7,11 @@ import MySQLdb.cursors
 
 dbOps.mySqlSetErrorOnWarn()
 
-def connect(db="",  confFile=None, dictCursor=False):
+def connect(db="",  confFile=None, dictCursor=False, host=None):
     """connect to genome mysql server, using confFile or ~/.hg.conf"""
     conf = HgConf.obtain(confFile)
     cursorclass = MySQLdb.cursors.DictCursor if dictCursor else MySQLdb.cursors.Cursor
-    return MySQLdb.Connect(host=conf["db.host"], user=conf["db.user"], passwd=conf["db.password"], db=db, cursorclass=cursorclass)
+    if host == None:
+        host = conf["db.host"]
+    return MySQLdb.Connect(host=host, user=conf["db.user"], passwd=conf["db.password"], db=db, cursorclass=cursorclass)
     
