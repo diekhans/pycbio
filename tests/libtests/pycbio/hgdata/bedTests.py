240b526cfe56a02800904335b3f5f9123fe8c7b2
Mark Diekhans <markd@soe.ucsc.edu>
2017-09-13 22:31:59 -0700

    added test for writing bed

--- tests/libtests/pycbio/hgdata/bedTests.py
+++ tests/libtests/pycbio/hgdata/bedTests.py
@@ -16,6 +16,13 @@ class BedTests(TestCaseBase):
         self.assertEquals(str(hits[0]),
                           "chr12	119575618	119589763	NM_000017.1	0	+	119575641	119589204	0	10	69,164,150,112,152,171,138,96,57,712,	0,1163,11123,11493,11974,12417,12670,12957,13277,13433,")
 
+    def testWriteBed(self):
+        beds = BedTable(self.getInputFile("fromPslMinTest.bed"))
+        outBedFile = self.getOutputFile(".bed")
+        with open(outBedFile, "w") as outFh:
+            for bed in beds:
+                bed.write(outFh)
+        self.diffFiles(self.getInputFile("fromPslMinTest.bed"), outBedFile)
 
 def suite():
     ts = unittest.TestSuite()
