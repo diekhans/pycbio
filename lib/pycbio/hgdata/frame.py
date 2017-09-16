2243505c4186eda3b015dba40f04eb8f4816ef03
Mark Diekhans <markd@soe.ucsc.edu>
2017-09-15 20:51:21 -0700

    simplied Frame

--- lib/pycbio/hgdata/frame.py
+++ lib/pycbio/hgdata/frame.py
@@ -1,39 +1,35 @@
 # Copyright 2006-2012 Mark Diekhans
 
-# FIXME: should None just be uses as no frame?
-
 
 class Frame(int):
-    """Immutable object the represents a frame, integer value of 0, 1, 2, or
-    -1 for no frame.  This is also an int"""
+    """Immutable object the represents a frame, integer value of 0, 1, or 2.
+    This is also an int.  Use None if there is no frame."""
 
     __slots__ = ()
 
     @staticmethod
     def __checkFrameValue(val):
-        if not ((val >= -1) and (val <= 2)):
-            raise TypeError("frame must be an integer in the range -1..2, got {}".format(val))
+        if not ((val >= 0) and (val <= 2)):
+            raise TypeError("frame must be an integer in the range 0..2, got {}".format(val))
 
-    def __new__(cls, val=-1):
+    def __new__(cls, val):
         obj = super(Frame, cls).__new__(cls, val)
         cls.__checkFrameValue(obj)
         return obj
 
     @staticmethod
     def fromPhase(phase):
-        """construct a Frame from a GFF/GTF like phase"""
+        """construct a Frame from a GFF/GTF like phase, which maybe an int or str"""
         if isinstance(phase, str):
             phase = int(phase)
-        if phase < 0:
-            return Frame(-1)
-        elif phase == 0:
+        if phase == 0:
             return Frame(0)
         elif phase == 1:
             return Frame(2)
         elif phase == 2:
             return Frame(1)
         else:
-            raise Exception("invalid phase: " + str(phase))
+            raise Exception("invalid phase: {}".format(phase))
 
     def toPhase(self):
         """frame expressed as a GFF/GTF like phase, which is the number of bases
@@ -42,29 +38,23 @@ class Frame(int):
             return 0
         elif self == 1:
             return 2
-        elif self == 2:
-            return 1
         else:
-            return -1
+            return 1
 
     def incr(self, amt):
         """increment frame by positive or negative amount, returning a new
             Frame object.  Frame of -1 already returns -1."""
-        val = int(self)
-        if val < 0:
-            return self  # no frame, not changed
-        elif val >= 0:
+        if not isinstance(amt, int):
+            raise ValueError("can only increment a frame by an int value: {} {}".format(type(amt), amt))
+        val = int(self)  # prevent infinite recursion
+        if amt >= 0:
             return Frame((val + amt) % 3)
         else:
             amt3 = (-amt) % 3
             return Frame((val - (amt - amt3)) % 3)
 
     def __add__(self, amt):
-        if not isinstance(amt, int):
-            return NotImplemented
         return self.incr(amt)
 
     def __sub__(self, amt):
-        if not isinstance(amt, int):
-            return NotImplemented
         return self.incr(-amt)
--- tests/libtests/pycbio/hgdata/frameTests.py
+++ tests/libtests/pycbio/hgdata/frameTests.py
@@ -11,7 +11,6 @@ from pycbio.hgdata.frame import Frame
 class FrameTests(TestCaseBase):
     def testConstruct(self):
         self.assertEqual(Frame(1), 1)
-        self.assertEqual(Frame(-1), -1)
         self.assertEqual(Frame("1"), Frame(1))
 
     def testIncr(self):
@@ -21,7 +20,8 @@ class FrameTests(TestCaseBase):
         self.assertEqual(Frame(1) + 1, 2)
         self.assertEqual(Frame(1) - 1, 0)
         self.assertEqual(Frame(1) + 4, 2)
-        self.assertEqual(Frame(-1) + 1, -1)
+        self.assertEqual(Frame(1) - 6, 1)
+        self.assertEqual(Frame(1) - 7, 0)
 
     def testPhase(self):
         self.assertEqual(Frame.fromPhase(0), Frame(0))
