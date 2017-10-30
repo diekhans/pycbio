2e8b0a2e8c8b988e7cb6c3269acd5ead3b6b6ee5
Mark Diekhans <markd@soe.ucsc.edu>
2017-10-30 09:02:38 -0700

    added remove function

--- lib/pycbio/hgdata/rangeFinder.py
+++ lib/pycbio/hgdata/rangeFinder.py
@@ -16,6 +16,12 @@ generating SQL where clauses to restrict by bin."""
 # will fit in.
 from __future__ import print_function
 from builtins import range
+from collections import namedtuple
+
+def RemoveValueError(ValueError):
+    "error when removed not found"
+    def __init__(self, start, end):
+        super(RemoveValueError, self).__init__("range {}-{} with specified value not found".format(start, end))
 
 
 class Binner(object):
@@ -46,7 +52,7 @@ class Binner(object):
                 return baseOffset + binOff + startBin
             startBin >>= Binner.binNextShift
             endBin >>= Binner.binNextShift
-        raise Exception("can't compute bin: start %d, end %d out of range" % (start, end))
+        raise Exception("can't compute bin: start {}, end {} out of range".format(start, end))
 
     @staticmethod
     def calcBin(start, end):
@@ -68,7 +74,7 @@ class Binner(object):
 
     @staticmethod
     def getOverlappingBins(start, end):
-        """Generate of bins for the range.  Each value is closed range of (startBin, endBin)"""
+        """Generate bins for the range.  Each value is closed range of (startBin, endBin)"""
         if end <= Binner.binBasicMaxEnd:
             # contained in basic range
             for bins in Binner.__getOverlappingBinsForOffsets(start, end, 0, Binner.binOffsetsBasic):
@@ -95,14 +101,10 @@ class Binner(object):
         return "(({}=\"{}\") and ({}<{}) and ({}>{}) and ({}))".format(seqCol, seq, startCol, end, endCol, start, " or ".join(parts))
 
 
-class Entry(object):
+class Entry(namedtuple("Entry",
+                       ("start", "end", "value"))):
     "entry associating a range with a value"
-    __slots__ = ("start", "end", "value")
-
-    def __init__(self, start, end, value):
-        self.start = start
-        self.end = end
-        self.value = value
+    __slots__ = ()
 
     def overlaps(self, start, end):
         "test if the range is overlapped by the entry"
@@ -116,18 +118,18 @@ class RangeBins(object):
     """Range indexed container for a single sequence.  This using a binning
     scheme that implements spacial indexing. Based on UCSC hg browser binRange
     C module.  """
-    __slots__ = ("seqId", "strand", "bins")
+    __slots__ = ("seqId", "strand", "buckets")
 
     def __init__(self, seqId, strand):
         self.seqId = seqId
         self.strand = strand
-        self.bins = {}  # indexed by bin
+        self.buckets = {}  # indexed by bin, each bucket is a list
 
     def add(self, start, end, value):
         bin = Binner.calcBin(start, end)
-        entries = self.bins.get(bin)
-        if (entries is None):
-            self.bins[bin] = entries = []
+        entries = self.buckets.get(bin)
+        if entries is None:
+            self.buckets[bin] = entries = []
         entries.append(Entry(start, end, value))
 
     def overlapping(self, start, end):
@@ -135,23 +137,32 @@ class RangeBins(object):
         if (start < end):
             for bins in Binner.getOverlappingBins(start, end):
                 for j in range(bins[0], bins[1] + 1):
-                    bin = self.bins.get(j)
-                    if (bin is not None):
-                        for entry in bin:
+                    bucket = self.buckets.get(j)
+                    if bucket is not None:
+                        for entry in bucket:
                             if entry.overlaps(start, end):
                                 yield entry.value
 
+    def removeIfExists(self, start, end, value):
+        """Remove an entry with the particular range if it exists, otherwise return false """
+        try:
+            bucket = self.buckets[(Binner.calcBin(start, end))]  # exception if no bucket
+            bucket.remove(Entry(start, end, value))  # exception if no value
+            return True
+        except IndexError, ValueError:
+            return False
+
     def values(self):
         "generator over all values"
-        for bin in list(self.bins.values()):
+        for bin in list(self.buckets.values()):
             for entry in bin:
                 yield entry.value
 
     def dump(self, fh):
         "print contents for debugging purposes"
-        for bin in list(self.bins.keys()):
+        for bin in list(self.buckets.keys()):
             fh.write(self.seqId + " (" + str(self.strand) + ") bin=" + str(bin) + "\n")
-            for entry in self.bins[bin]:
+            for entry in self.buckets[bin]:
                 fh.write("\t" + str(entry) + "\n")
 
 
@@ -167,46 +178,86 @@ class RangeFinder(object):
         self.haveStrand = None
         self.seqBins = {}
 
+    def __checkStrand(self, strand):
+        if strand not in (None, "+", "-"):
+            raise Exception("invalid strand: {}".format(strand))
+        
     def add(self, seqId, start, end, value, strand=None):
         "add an entry for a sequence and range, and optional strand"
+        self.__checkStrand(strand)
         if self.haveStrand is None:
             self.haveStrand = (strand is not None)
         elif self.haveStrand != (strand is not None):
             raise Exception("all RangeFinder entries must either have strand or not have strand")
-        if strand not in self.validStrands:
-            raise Exception("invalid strand: " + str(strand))
         key = (seqId, strand)
         bins = self.seqBins.get(key)
         if bins is None:
             self.seqBins[key] = bins = RangeBins(seqId, strand)
         bins.add(start, end, value)
 
+    def __overlappingSpecificStrand(self, seqId, start, end, strand):
+        "check overlap on specific strand, which might be None"
+        bins = self.seqBins.get((seqId, strand))
+        if bins is not None:
+            for value in bins.overlapping(start, end):
+                yield value
+        
+    def __overlappingBothStrands(self, seqId, start, end):
+        "return range overlaps, checking both strands"
+        for value in self.__overlappingSpecificStrand(seqId, start, end, '+'):
+            yield value
+        for value in self.__overlappingSpecificStrand(seqId, start, end, '-'):
+            yield value
+
     def overlapping(self, seqId, start, end, strand=None):
         "generator over values overlaping the specified range on seqId, optional strand"
-        if strand not in self.validStrands:
-            raise Exception("invalid strand: " + str(strand))
+        self.__checkStrand(strand)
         if self.haveStrand and (strand is None):
-            # must try both strands
-            bins = self.seqBins.get((seqId, "+"))
-            if bins is not None:
-                for value in bins.overlapping(start, end):
-                    yield value
-            bins = self.seqBins.get((seqId, "-"))
-            if bins is not None:
-                for value in bins.overlapping(start, end):
-                    yield value
+            # must check on both strands
+            return self.__overlappingBothStrands(seqId, start, end)
         else:
             # must only check a specifc strand, or no strand to check
             if not self.haveStrand:
                 strand = None  # no strand to check
-            bins = self.seqBins.get((seqId, strand))
+            return self.__overlappingSpecificStrand(seqId, start, end, strand)
+
+    def __removeIfExists(self, seqId, start, end, value, strand):
+        removed = False
+        bins = self.seqBins.get((seqId, strand))
+        if bins is not None:
+            removed = bins.removeIfExists(start, end, value)
+        return removed
+
+    def __removeSpecificStrand(self, seqId, start, end, value, strand):
+        "remove an entry on specific strand, which might be None"
+        if not self.__removeIfExists(seqId, start, end, value, strand):
+            raise RemoveValueError(start, end)
+            
+    def __removeBothStrands(self, seqId, start, end, value):
+        "remove an entry, checking both strands"
+        removed = self.__removeIfExists(seqId, start, end, value, '+')
+        if not removed:
+            removed = self.__removeIfExists(seqId, start, end, value, '-')
             if bins is not None:
-                for value in bins.overlapping(start, end):
-                    yield value
+                removed = bins.removeIfExists(seqId, start, end, value)
+        if not removed:
+            raise RemoveValueError(start, end)
+        
+    def remove(self, seqId, start, end, value, strand=None):
+        """remove an entry with the particular range and value, value error if not found"""
+        self.__checkStrand(strand)
+        if self.haveStrand and (strand is None):
+            # must check on both strands
+            self.__removeBothStrands(seqId, start, end, value)
+        else:
+            # must only check a specifc strand, or no strand to check
+            if not self.haveStrand:
+                strand = None  # no strand to check
+            self.__removeSpecificStrand(seqId, start, end, value, strand)
 
     def values(self):
         "generator over all values"
-        for bins in self.seqBins:
+        for bins in self.seqBins.values():
             for value in list(bins.values()):
                 yield value
 
--- tests/libtests/pycbio/hgdata/rangeFinderTests.py
+++ tests/libtests/pycbio/hgdata/rangeFinderTests.py
@@ -5,41 +5,45 @@ if __name__ == '__main__':
     sys.path.append("../../../../lib")
 from pycbio.sys.testCaseBase import TestCaseBase
 from pycbio.hgdata.rangeFinder import RangeFinder
+from collections import namedtuple
 
 debug = True
 
-# (seqId start end strand value)
+# test data and query types
+Data = namedtuple("Data", ("seqId", "start", "end", "strand", "value"))
+Query = namedtuple("Query", ("seqId", "start", "end", "strand", "expectWithStrand", "expectWithOutStrand"))
+
 data1 = (
-    ("chr22", 100, 1000, '+', "val1.1"),
-    ("chr12", 100, 1000, '+', "val1.2"),
-    ("chr12", 100, 500, '-', "val1.3"),
-    ("chr12", 150, 10000, '+', "val1.4"),
-    ("chr32", 1000000000, 2000000000, '+', "val1.5"),  # outside of basic range
-    ("chr32", 100000, 2000000000, '-', "val1.6"),  # crossing of basic and extended range
+    Data("chr22", 100, 1000, '+', "val1.1"),
+    Data("chr12", 100, 1000, '+', "val1.2"),
+    Data("chr12", 100, 500, '-', "val1.3"),
+    Data("chr12", 150, 10000, '+', "val1.4"),
+    Data("chr32", 1000000000, 2000000000, '+', "val1.5"),  # outside of basic range
+    Data("chr32", 100000, 2000000000, '-', "val1.6"),  # crossing of basic and extended range
 )
 # (seqId start end strand expectWithStrand expectWithoutStrand)
 queries1 = (
-    ("chr20", 100, 1000, '+', (), ()),
-    ("chr22", 100, 1000, '+', ("val1.1",), ("val1.1",)),
-    ("chr22", 100, 1000, '-', (), ("val1.1",)),
-    ("chr22", 110, 111, '+', ("val1.1",), ("val1.1",)),
-    ("chr22", 110, 111, '-', (), ("val1.1",)),
-    ("chr12", 1, 150, '-', ("val1.3",), ("val1.2", "val1.3")),
-    ("chr12", 10000, 1500000, '+', (), ()),
-    ("chr12", 1, 151, '-', ("val1.3",), ("val1.2", "val1.3", "val1.4")),
-    ("chr12", 1, 151, '+', ("val1.2", "val1.4"), ("val1.2", "val1.3", "val1.4")),
-    ("chr32", 10, 100001, '+', (), ("val1.6",)),
-    ("chr32", 10, 1000000001, '+', ("val1.5",), ("val1.5", "val1.6",)),
-    ("chr32", 1900000001, 2000000002, '+', ("val1.5",), ("val1.5", "val1.6",)),
+    Query("chr20", 100, 1000, '+', (), ()),
+    Query("chr22", 100, 1000, '+', ("val1.1",), ("val1.1",)),
+    Query("chr22", 100, 1000, '-', (), ("val1.1",)),
+    Query("chr22", 110, 111, '+', ("val1.1",), ("val1.1",)),
+    Query("chr22", 110, 111, '-', (), ("val1.1",)),
+    Query("chr12", 1, 150, '-', ("val1.3",), ("val1.2", "val1.3")),
+    Query("chr12", 10000, 1500000, '+', (), ()),
+    Query("chr12", 1, 151, '-', ("val1.3",), ("val1.2", "val1.3", "val1.4")),
+    Query("chr12", 1, 151, '+', ("val1.2", "val1.4"), ("val1.2", "val1.3", "val1.4")),
+    Query("chr32", 10, 100001, '+', (), ("val1.6",)),
+    Query("chr32", 10, 1000000001, '+', ("val1.5",), ("val1.5", "val1.6",)),
+    Query("chr32", 1900000001, 2000000002, '+', ("val1.5",), ("val1.5", "val1.6",)),
 )
 
 # potential regression with exact range matches
 data2 = (
-    ("chr1", 100316598, 100387207, '+', "NM_000643.2"),
-    ("chr1", 100316598, 100387207, '+', "NM_000644.2"),
+    Data("chr1", 100316598, 100387207, '+', "NM_000643.2"),
+    Data("chr1", 100316598, 100387207, '+', "NM_000644.2"),
 )
 queries2 = (
-    ("chr1", 100316598, 100387207, '+', ("NM_000643.2", "NM_000644.2"), ("NM_000643.2", "NM_000644.2")),
+    Query("chr1", 100316598, 100387207, '+', ("NM_000643.2", "NM_000644.2"), ("NM_000643.2", "NM_000644.2")),
 )
 
 
@@ -48,14 +52,14 @@ class RangeTests(TestCaseBase):
         rf = RangeFinder()
         for row in data:
             if useStrand:
-                rf.add(row[0], row[1], row[2], row[4], row[3])
+                rf.add(row.seqId, row.start, row.end, row.value, row.strand)
             else:
-                rf.add(row[0], row[1], row[2], row[4], None)
+                rf.add(row.seqId, row.start, row.end, row.value)
         return rf
 
     def failTrace(self, desc, query, expect, got):
-        sys.stderr.write(("%s query failed: %s:%d-%d %s: expect: %s\n\tgot: %s\n"
-                          % (desc, query[0], query[1], query[2], query[3], str(expect), str(got))))
+        msg = "{} query failed: {}:{}-{} {}: expect: {}\n\tgot: {}\n".format(desc, query.seqId, query.start, query.end, query.strand, str(expect), str(got))
+        sys.stderr.write(msg)
         sys.stderr.flush()
 
     def doQuery(self, rf, seqId, start, end, strand):
@@ -66,19 +70,19 @@ class RangeTests(TestCaseBase):
 
     def doStrandQuery(self, rf, query):
         if rf.haveStrand:
-            expect = query[4]
+            expect = query.expectWithStrand
         else:
-            expect = query[5]
-        val = self.doQuery(rf, query[0], query[1], query[2], query[3])
+            expect = query.expectWithOutStrand
+        val = self.doQuery(rf, query.seqId, query.start, query.end, query.strand)
         if debug and (val != expect):
             self.failTrace("strand of haveStrand={}".format(rf.haveStrand), query, expect, val)
         self.assertEqual(val, expect)
 
     def doNoStrandQuery(self, rf, query):
-        expect = query[5]
-        val = self.doQuery(rf, query[0], query[1], query[2], None)
+        expect = query.expectWithOutStrand
+        val = self.doQuery(rf, query.seqId, query.start, query.end, None)
         if debug and (val != expect):
-            self.failTrace("nostrand of haveStrand={}".format(rf.haveStrand), query, expect, val)
+            self.failTrace("no-strand of haveStrand={}".format(rf.haveStrand), query, expect, val)
         self.assertEqual(val, expect)
 
     def doQueries(self, rf, queries, useStrand):
@@ -113,6 +117,23 @@ class RangeTests(TestCaseBase):
         rf = self.mkRangeFinder(data2, True)
         self.doQueries(rf, queries2, True)
         self.doQueries(rf, queries2, False)
+
+
+    def testRemoveNoStrand(self):
+        "exact range matches"
+        rf = self.mkRangeFinder(data1, False)
+        ent = data1[1]  # val1.2
+        rf.remove(ent.seqId, ent.start, ent.end, ent.value)
+        left = sorted(rf.values())
+        expect = ['val1.1', 'val1.3', 'val1.4', 'val1.5', 'val1.6']
+
+    def testRemoveStrand(self):
+        "exact range matches"
+        rf = self.mkRangeFinder(data1, False)
+        ent = data1[1]  # val1.2
+        rf.remove(ent.seqId, ent.start, ent.end, ent.value, ent.strand)
+        left = sorted(rf.values())
+        expect = ['val1.1', 'val1.3', 'val1.4', 'val1.5', 'val1.6']
 
 
 def suite():
