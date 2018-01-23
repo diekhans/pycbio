605e18f8ec4a5428d4cf6e5e3b578b291c39f711
Mark Diekhans <markd@soe.ucsc.edu>
2018-01-23 15:54:55 -0800

    added cpu to parasol

--- lib/pycbio/distrib/parasol.py
+++ lib/pycbio/distrib/parasol.py
@@ -82,7 +82,7 @@ class BatchStats(object):
 
 class Para(object):
     "interface to the parasol para command"
-    def __init__(self, paraHost, runDir, paraDir, jobFile=None, mem=None):
+    def __init__(self, paraHost, runDir, paraDir, jobFile=None, cpu=None, mem=None):
         """"will chdir to run dir.. paraDir should be relative
         to runDir or absolute, jobFile should be relative to runDir
         or absolute.
@@ -92,6 +92,7 @@ class Para(object):
         self.runDir = os.path.realpath(os.path.abspath(runDir))
         self.paraDir = os.path.realpath(paraDir)
         self.jobFile = jobFile
+        self.cpu = cpu
         self.mem = mem
         fileOps.ensureDir(self.__mkAbs(self.runDir, self.paraDir))
         if jobFile is not None:
@@ -119,6 +120,8 @@ class Para(object):
         paraCmd = ["para", "-batch={}".format(shlex_quote(self.paraDir))]
         for pa in paraArgs:
             paraCmd.append(shlex_quote(pa))
+        if self.cpu is not None:
+            paraCmd.append("-cpu={}".format(self.cpu))
         if self.mem is not None:
             paraCmd.append("-ram={}".format(self.mem))
         remCmd = "cd {} && {}".format(shlex_quote(self.runDir), " ".join(paraCmd))
