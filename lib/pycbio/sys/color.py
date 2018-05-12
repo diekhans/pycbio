1cc94339f2a50dbd38c3cb2a28ad4685f873cc90
Mark Diekhans <markd@soe.ucsc.edu>
2018-05-11 23:32:23 -0700

    added comments

--- lib/pycbio/sys/color.py
+++ lib/pycbio/sys/color.py
@@ -5,6 +5,8 @@ from builtins import round
 import colorsys
 from collections import namedtuple
 
+# FIXME: add optional alpha.
+# FIXME: do we really need to store both RGB and HSV?
 
 class Color(namedtuple("Color", ("red", "green", "blue",
                                  "hue", "saturation", "value"))):
