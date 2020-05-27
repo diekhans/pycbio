320ab13c11b914764f8cdf67aef5cfdcc13803d4
Mark Diekhans <markd@ucsc.edu>
2020-05-26 18:39:16 -0700

    added function for accessing bigBeds

---
+++ lib/pycbio/hgdata/bigBedAccessor.py
@@ -0,0 +1,49 @@
+# Copyright 2006-2020 Mark Diekhans
+import pyBigWig
+from pycbio.hgdata.bed import Bed
+
+class BigBedAccessor(object):
+    """Access a bigBed file either randomly or sequentially. It can be
+    used as a context manager."""
+    def __init__(self, bigBedFile, numStdCols=None):
+        self.bigBedFile = bigBedFile
+        self.bigBedFh = pyBigWig.open(bigBedFile)
+        self.numStdCols = numStdCols
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.close()
+
+    def close(self):
+        if self.bigBedFh is not None:
+            try:
+                self.bigBedFh.close()
+            finally:
+                self.bigBedFh = None
+
+    def chromNames(self):
+        return list(sorted(self.bigBedFh.chroms().keys()))
+
+    def getChromSize(self, chrom):
+        "get size or none if chrom does not exist"
+        return self.bigBedFh.chroms().get(chrom)
+
+    def _recToBed(self, chrom, entry):
+        row = [chrom, str(entry[0]), str(entry[1])] + entry[2].split('\t')
+        return Bed.parse(row, self.numStdCols)
+
+    def overlapping(self, chrom, start, end):
+        if chrom in self.bigBedFh.chroms():
+            for entry in self.bigBedFh.entries(chrom, start, end):
+                yield self._recToBed(chrom, entry)
+
+    def __iter__(self):
+        "iterating is not memory efficient"
+        def gen(self):
+            for chrom in self.chromNames():
+                chromSize = self.getChromSize(chrom)
+                for entry in self.bigBedFh.entries(chrom, 0, chromSize):
+                    yield self._recToBed(chrom, entry)
+        return gen(self)
---
+++ tests/libtests/pycbio/hgdata/bigBedAccessorTest.py
@@ -0,0 +1,46 @@
+# Copyright 2006-2020 Mark Diekhans
+import unittest
+import sys
+if __name__ == '__main__':
+    sys.path.insert(0, "../../../../lib")
+from pycbio.sys.testCaseBase import TestCaseBase
+from pycbio.hgdata.bigBedAccessor import BigBedAccessor
+from pycbio.hgdata.bed import BedReader
+
+# note: test bigBed are check in as a binary to avoid need to have tools
+# and chrom files to convert on the file.  Recreate with:
+#
+#  bedToBigBed -tab -type=bed12+4 input/bed12extra.bed /hive/data/genomes/hg38/chrom.sizes input/bed12extra.bigBed
+#
+
+class BigBedAccessorTests(TestCaseBase):
+    def _loadBed12Extra(self):
+        return tuple(BedReader(self.getInputFile("bed12extra.bed"), numStdCols=4))
+
+    def _assertBeds(self, beds, bigBeds):
+        self.assertEqual(len(beds), len(bigBeds))
+        for i in range(len(beds)):
+            self.assertEqual(str(beds[i]), str(bigBeds[i]))
+
+    def testBed12Iter(self):
+        with BigBedAccessor(self.getInputFile("bed12extra.bigBed"), numStdCols=12) as fh:
+            fromBigBed = [b for b in fh]
+        self._assertBeds(self._loadBed12Extra(), fromBigBed)
+
+    def testBed12Rand(self):
+        with BigBedAccessor(self.getInputFile("bed12extra.bigBed"), numStdCols=12) as fh:
+            fromBigBed = []
+            bed12extra = self._loadBed12Extra()
+            for bed in bed12extra:
+                for bb in fh.overlapping(bed.chrom, bed.start, bed.end):
+                    fromBigBed.append(bb)
+        self._assertBeds(bed12extra, fromBigBed)
+
+def suite():
+    ts = unittest.TestSuite()
+    ts.addTest(unittest.makeSuite(BigBedAccessorTests))
+    return ts
+
+
+if __name__ == '__main__':
+    unittest.main()
