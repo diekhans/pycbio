4f3cb0c1bd635c772b2e19fe2b106adc75b00bbc
Mark Diekhans <markd@ucsc.edu>
2021-05-14 20:40:29 -0700

    added gencode biotype defintions from other sources

---
+++ tests/libtests/pycbio/gencode/gencodeTests.py
@@ -0,0 +1,21 @@
+# Copyright 2006-2012 Mark Diekhans
+import unittest
+import sys
+if __name__ == '__main__':
+    sys.path.insert(0, "../../../../lib")
+from pycbio.sys.testCaseBase import TestCaseBase
+from pycbio.gencode import biotypes
+
+class GencodeTests(TestCaseBase):
+    # asserts in biotypes test a lot, should move into here
+    pass
+
+
+def suite():
+    ts = unittest.TestSuite()
+    ts.addTest(unittest.makeSuite(GencodeTests))
+    return ts
+
+
+if __name__ == '__main__':
+    unittest.main()
