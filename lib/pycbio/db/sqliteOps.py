15d810bbdb86923e743b54f1ebb77ec8360c241d
Mark Diekhans <markd@ucsc.edu>
2022-07-21 01:52:04 -0700

    make sqllite objDictRowFactory the default

--- lib/pycbio/db/sqliteOps.py
+++ lib/pycbio/db/sqliteOps.py
@@ -11,11 +11,17 @@ from pycbio.sys.objDict import ObjDict
 # FIXME  gets Obj of rows:
 
 
-def connect(sqliteDb, create=False, readonly=True, timeout=None, synchronous=None, rowFactory=None):
+def objDictRowFactory(cur, row):
+    """Row factory to return a ObjDict of the result.  Note that non-unique
+    names from multiple tables will end up with the last column of the same
+    name winning.  In sane cases, these will have the same value"""
+    return ObjDict(zip((t[0] for t in cur.description), row))
+
+def connect(sqliteDb, create=False, readonly=True, timeout=None, synchronous=None, rowFactory=objDictRowFactory):
     """Connect to an sqlite3 database.  If create is specified, then database
     is created if it doesn't exist.  If sqliteDb is None, and in-memory data
     is created with create and readonly flags being ignored. If create is True,
-    readonly is set to False."""
+    readonly is set to False.  Set rowFactory to None to get default tuple row factory."""
     if create:
         readonly = False
     if sqliteDb is None:
@@ -45,12 +51,6 @@ def setSynchronous(conn, mode):
     elif mode is True:
         mode = "NORMAL"
     execute(conn, "PRAGMA synchronous={}".format(mode))
-
-def objDictRowFactory(cur, row):
-    """Row factory to return a ObjDict of the result.  Note that non-unique
-    names from multiple tables will end up with the last column of the same
-    name winning.  In sane cases, these will have the same value"""
-    return ObjDict(zip((t[0] for t in cur.description), row))
 
 class SqliteCursor(object):
     """Context manager creating an sqlite cursor and a transaction.
