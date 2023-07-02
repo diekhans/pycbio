7a7415b7d70bae22817ad83cd4fa725312301027
Mark Diekhans <markd@ucsc.edu>
2023-07-01 18:49:01 -0700

    add dumping profile report on exit

--- lib/pycbio/sys/profile.py
+++ lib/pycbio/sys/profile.py
@@ -3,6 +3,7 @@ import cProfile
 import signal
 import argparse
 import sys
+import atexit
 from pycbio import PycbioException
 
 
@@ -14,35 +15,32 @@ class Profile:
     This works with both optparse and argparse.
 
     Serving suggestion:
-        parser = OptionParser(usage=CmdOpts.usage)
+        parser = argparse.ArgumentParser(description=desc)
         profiler = Profile(parser)
         ...
-        (opts, args) = parser.parse_args()
+        opts = parser.parse_args()
         ...
         profiler.setup(opts)
 
     at the end of the program:
-        profiler.finishUp()
+        profiler.finishup()
+    or let exit handle call finishup()
 
     Use the program profStats to create reports.
     """
 
     def __init__(self, cmdParser):
-        profileHelp = "enable profiling, logging to this file"
-        profileSignalHelp = "specify signal number that will stop logging and exit program"
-        if isinstance(cmdParser, argparse.ArgumentParser):
-            cmdParser.add_argument("--profile", dest="profile", action="store", default=None, help=profileHelp)
-            cmdParser.add_argument("--profile-signal", dest="signal", action="store", default=None, type=int, help=profileSignalHelp)
-        else:
-            cmdParser.add_option("--profile", dest="profile", action="store", default=None, help=profileHelp)
-            cmdParser.add_option("--profile-signal", dest="signal", action="store", default=None, type="int", help=profileSignalHelp)
+        cmdParser.add_argument("--profile",
+                               help="enable profiling, logging to this file")
+        cmdParser.add_argument("--profile-signal", dest="profileSignal", default=None, type=int,
+                               help="specify signal number that will stop logging and exit program")
         self.profiler = None
         self.logFile = None
         self.signum = None
 
     def _sigHandler(self, signum, frame):
         "signal handler to stop logging and terminate process"
-        self.finishUp()
+        self.finishup()
         sys.stderr("Warning: profiler exiting on signal\n")
         sys.exit(1)
 
@@ -53,11 +51,12 @@ class Profile:
     def setup(self, opts):
         """initializing profiling, if requested"""
         if opts.profile is None:
-            if opts.signal is not None:
+            if opts.profileSignal is not None:
                 raise PycbioException("can't specify --profile-signal without --profile")
         else:
-            if opts.signal is not None:
-                self._setupSignalHandler(opts.signal)
+            if opts.profileSignal is not None:
+                self._setupSignalHandler(opts.profileSignal)
+            atexit.register(self.finishup)
             self.logFile = opts.profile
             self.profiler = cProfile.Profile()
             self.profiler.enable()
@@ -66,7 +65,7 @@ class Profile:
         signal.signal(self.signum, signal.SIG_IGN)
         self.signum = None
 
-    def finishUp(self):
+    def finishup(self):
         "if profiling is enabled, stop and close log file"
         if self.profiler is not None:
             self.profiler.disable()
