4f3831af67ac82b9ceaeb8c955350cb6dfb2ef04
Mark Diekhans <markd@ucsc.edu>
2024-08-14 22:00:52 -0700

    added getOptionalArgs

---
+++ lib/pycbio/sys/cmdOps.py
@@ -0,0 +1,14 @@
+# Copyright 2006-2022 Mark Diekhans
+"""Miscellaneous command line parsing operations"""
+from pycbio.sys.objDict import ObjDict
+
+def getOptionalArgs(parser, args):
+    """Get the parse command line option arguments (-- or - options) as an
+    object were the options are fields in the object.  Useful for packaging up
+    a large number of options to pass around."""
+
+    opts = ObjDict()
+    for act in parser._actions:
+        if act.option_strings[0].startswith('-') and (act.dest != 'help'):
+            setattr(opts, act.dest, getattr(args, act.dest))
+    return opts
---
+++ tests/libtests/pycbio/sys/test_cmdOps.py
@@ -0,0 +1,53 @@
+# Copyright 2006-2022 Mark Diekhans
+"""Miscellaneous command line parsing operations tests"""
+import sys
+import unittest
+import argparse
+if __name__ == '__main__':
+    sys.path.insert(0, "../../../../lib")
+from pycbio.sys.testCaseBase import TestCaseBase
+from pycbio.sys import cmdOps
+
+class TestArgParser(argparse.ArgumentParser):
+    "raises exception rather than exit"
+    def error(self, message):
+        raise argparse.ArgumentError(None, message)
+
+def makeTrekParser():
+    parser = TestArgParser(description='to go where no one has gone before')
+    parser.add_argument('--kirk', type=int)
+    parser.add_argument('--spock', '-s', type=str)
+    parser.add_argument('-m', '--mc-coy', dest='mc_coy', type=str)
+    parser.add_argument('-u', dest='uhura', action='store_true')
+    return parser
+
+class CmdOpsTests(TestCaseBase):
+    def _checkTrekOpts(self, opts, kirk, spock, mc_coy, uhura):
+        self.assertEqual(opts.kirk, kirk)
+        self.assertEqual(opts.spock, spock)
+        self.assertEqual(opts.mc_coy, mc_coy)
+        self.assertEqual(opts.uhura, uhura)
+
+    def testGetOptsLong(self):
+        parser = makeTrekParser()
+        testargs = ('--kirk=10', '--spock=fred')
+        args = parser.parse_args(testargs)
+        opts = cmdOps.getOptionalArgs(parser, args)
+        self._checkTrekOpts(opts, 10, 'fred', None, False)
+
+    def testGetOptsShort(self):
+        parser = makeTrekParser()
+        testargs = ('-s' 'fred', '-m', 'barney', '-u')
+        args = parser.parse_args(testargs)
+        opts = cmdOps.getOptionalArgs(parser, args)
+        self._checkTrekOpts(opts, None, 'fred', 'barney', True)
+
+
+def suite():
+    ts = unittest.TestSuite()
+    ts.addTest(unittest.makeSuite(CmdOpsTests))
+    return ts
+
+
+if __name__ == '__main__':
+    unittest.main()
