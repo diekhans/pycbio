8b799f7fd8b5cddec9247acfde23e52e6c84d924
Mark Diekhans <markd@ucsc.edu>
2025-06-08 00:21:47 -0700

    test_loggingOps.py to pytest

--- tests/libtests/pycbio/sys/test_mathOps.py
+++ tests/libtests/pycbio/sys/test_mathOps.py
@@ -1,67 +1,53 @@
 # Copyright 2006-2025 Mark Diekhans
-import unittest
 import sys
 import math
 if __name__ == '__main__':
     sys.path.insert(0, "../../../../lib")
 from pycbio.sys import mathOps
-from pycbio.sys.testCaseBase import TestCaseBase
 
 
-class RoundToOrderOfMagnitudeTests(TestCaseBase):
-    def _assertRound(self, expect, value):
-        "asserted that value is same as expect when rounded to it's order of magnitude"
-        # FIXME: could this be done another way?
-        self.assertEqual(expect, mathOps.roundToOrderOfMagnitude(value))
+def _assertRound(expect, value):
+    "asserted that value is same as expect when rounded to it's order of magnitude"
+    assert mathOps.roundToOrderOfMagnitude(value) == expect
 
-    def testMagnitudeFifty(self):
-        self._assertRound(100, 50)
+def testMagnitudeFifty():
+    _assertRound(100, 50)
 
-    def testMagnitudeFive(self):
-        self._assertRound(10, 5)
+def testMagnitudeFive():
+    _assertRound(10, 5)
 
-    def testMagnitudeZeroPointFive(self):
-        self._assertRound(1, 0.5)
+def testMagnitudeZeroPointFive():
+    _assertRound(1, 0.5)
 
-    def testMagnitudeZeroPointZeroFive(self):
-        self._assertRound(0.1, 0.05)
+def testMagnitudeZeroPointZeroFive():
+    _assertRound(0.1, 0.05)
 
-    def testMagnitudeZeroPointZeroZeroFive(self):
-        self._assertRound(0.01, 0.005)
+def testMagnitudeZeroPointZeroZeroFive():
+    _assertRound(0.01, 0.005)
 
-    def testMagnitudeNegativeFifty(self):
-        self._assertRound(-100, -50)
+def testMagnitudeNegativeFifty():
+    _assertRound(-100, -50)
 
-    def testMagnitudeNegativeFive(self):
-        self._assertRound(-10, -5)
+def testMagnitudeNegativeFive():
+    _assertRound(-10, -5)
 
-    def testMagnitudeNegativeZeroPointFive(self):
-        self._assertRound(-1, -0.5)
+def testMagnitudeNegativeZeroPointFive():
+    _assertRound(-1, -0.5)
 
-    def testMagnitudeNegativeZeroPointZeroFive(self):
-        self._assertRound(-0.1, -0.05)
+def testMagnitudeNegativeZeroPointZeroFive():
+    _assertRound(-0.1, -0.05)
 
-    def testMagnitudeNegativeZeroPointZeroZeroFive(self):
-        self._assertRound(-0.01, -0.005)
+def testMagnitudeNegativeZeroPointZeroZeroFive():
+    _assertRound(-0.01, -0.005)
 
-    def testMagnitudeZero(self):
-        self._assertRound(1, 0)
+def testMagnitudeZero():
+    _assertRound(1, 0)
 
-class TestMathOps(TestCaseBase):
-    def testSafeDivInt0(self):
-        self.assertTrue(math.isnan(mathOps.safeDiv(10, 0)))
+def testSafeDivInt0():
+    assert math.isnan(mathOps.safeDiv(10, 0))
 
-    def testSafeDivFloat0(self):
-        self.assertTrue(math.isnan(mathOps.safeDiv(10.1, 0.0)))
+def testSafeDivFloat0():
+    assert math.isnan(mathOps.safeDiv(10.1, 0.0))
 
-    def testSafeDivFloatNan(self):
-        self.assertTrue(math.isnan(mathOps.safeDiv(10.1, math.nan)))
-
-def suite():
-    ts = unittest.TestSuite()
-    ts.addTest(unittest.makeSuite(RoundToOrderOfMagnitudeTests))
-    return ts
-
-
-if __name__ == '__main__':
-    unittest.main()
+def testSafeDivFloatNan():
+    assert math.isnan(mathOps.safeDiv(10.1, math.nan))
