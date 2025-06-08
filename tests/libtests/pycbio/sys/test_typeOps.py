a4af09433f9e2d13800feeb3b4e3303420cc5db4
Mark Diekhans <markd@ucsc.edu>
2025-06-08 00:14:50 -0700

    test_typeOps to pytest

--- tests/libtests/pycbio/sys/test_typeOps.py
+++ tests/libtests/pycbio/sys/test_typeOps.py
@@ -1,43 +1,30 @@
 # Copyright 2006-2025 Mark Diekhans
-import unittest
 import sys
 if __name__ == '__main__':
     sys.path.insert(0, "../../../../lib")
-from pycbio.sys.testCaseBase import TestCaseBase
 from pycbio.sys import typeOps
 
 # FIXME: many more tests needed
 
 
-class TypeOpsTests(TestCaseBase):
-    def testAnnon(self):
-        ao = typeOps.annon(a=10, b=20, fred="spaced out")
-        self.assertEqual(ao.a, 10)
-        self.assertEqual(ao.b, 20)
-        self.assertEqual(str(ao), "a=10, b=20, fred='spaced out'")
+def testAnnon():
+    ao = typeOps.annon(a=10, b=20, fred="spaced out")
+    assert ao.a == 10
+    assert ao.b == 20
+    assert str(ao) == "a=10, b=20, fred='spaced out'"
 
-    def testAttrdic(self):
-        class ODict:
-            def __init__(self):
-                self.f = 100
+def testAttrdict():
+    class ODict:
+        def __init__(self):
+            self.f = 100
 
-        class OSlots:
-            __slots__ = ("f")
+    class OSlots:
+        __slots__ = ("f")
 
-            def __init__(self):
-                self.f = 101
+        def __init__(self):
+            self.f = 101
 
-        odict = ODict()
-        self.assertEqual(typeOps.attrdict(odict)['f'], 100)
-        oslots = OSlots()
-        self.assertEqual(typeOps.attrdict(oslots)['f'], 101)
-
-
-def suite():
-    ts = unittest.TestSuite()
-    ts.addTest(unittest.makeSuite(TypeOpsTests))
-    return ts
-
-
-if __name__ == '__main__':
-    unittest.main()
+    odict = ODict()
+    assert typeOps.attrdict(odict)['f'] == 100
+    oslots = OSlots()
+    assert typeOps.attrdict(oslots)['f'] == 101
