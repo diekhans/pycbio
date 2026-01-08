3a94ab747fd94328f01c07b3952bc887efcd5c10
Mark Diekhans <markd@ucsc.edu>
2026-01-08 07:08:10 -0800

    generated more BED tests

--- tests/libtests/pycbio/hgdata/test_bed.py
+++ tests/libtests/pycbio/hgdata/test_bed.py
@@ -5,7 +5,7 @@ import pickle
 if __name__ == '__main__':
     sys.path.insert(0, "../../../../lib")
 import pycbio.sys.testingSupport as ts
-from pycbio.hgdata.bed import Bed, BedBlock, BedTable, BedReader, bedFromPsl, bedMergeBlocks
+from pycbio.hgdata.bed import Bed, BedBlock, BedTable, BedReader, bedFromPsl, bedMergeBlocks, BedException
 from pycbio.sys.color import Color
 import pipettor
 
@@ -139,6 +139,118 @@ def testBedMerge1(request):
     mergedBed = bedMergeBlocks("test1", beds)
     assert mergedBed.toRow() == \
         ['chr1', '148682028', '148712560', 'test1', '0', '-', '148712560', '148712560', '0', '5', '151,335,582,854,304,', '0,1264,23019,26177,30228,']
+
+def testBedMergeOverlapping(request):
+    """Test merging BEDs with overlapping blocks"""
+    bed1 = Bed("chr1", 100, 300, "bed1", strand="+", blocks=[BedBlock(100, 150), BedBlock(200, 300)])
+    bed2 = Bed("chr1", 120, 350, "bed2", strand="+", blocks=[BedBlock(120, 180), BedBlock(250, 350)])
+    mergedBed = bedMergeBlocks("merged", [bed1, bed2])
+    assert mergedBed.chrom == "chr1"
+    assert mergedBed.chromStart == 100
+    assert mergedBed.chromEnd == 350
+    assert mergedBed.name == "merged"
+    assert mergedBed.strand == "+"
+    # Should merge into: [100-180, 200-350]
+    assert len(mergedBed.blocks) == 2
+    assert mergedBed.blocks[0] == BedBlock(100, 180)
+    assert mergedBed.blocks[1] == BedBlock(200, 350)
+
+def testBedMergeAdjacent(request):
+    """Test merging BEDs with adjacent blocks that should be joined"""
+    bed1 = Bed("chr2", 100, 250, "bed1", strand="-", blocks=[BedBlock(100, 150)])
+    bed2 = Bed("chr2", 150, 250, "bed2", strand="-", blocks=[BedBlock(150, 250)])
+    mergedBed = bedMergeBlocks("merged", [bed1, bed2])
+    # Adjacent blocks should merge into one
+    assert len(mergedBed.blocks) == 1
+    assert mergedBed.blocks[0] == BedBlock(100, 250)
+
+def testBedMergeNonOverlapping(request):
+    """Test merging BEDs with non-overlapping blocks"""
+    bed1 = Bed("chr3", 100, 200, "bed1", strand="+", blocks=[BedBlock(100, 150)])
+    bed2 = Bed("chr3", 300, 400, "bed2", strand="+", blocks=[BedBlock(300, 350)])
+    mergedBed = bedMergeBlocks("merged", [bed1, bed2])
+    assert mergedBed.chromStart == 100
+    assert mergedBed.chromEnd == 400
+    # Non-overlapping blocks should remain separate
+    assert len(mergedBed.blocks) == 2
+    assert mergedBed.blocks[0] == BedBlock(100, 150)
+    assert mergedBed.blocks[1] == BedBlock(300, 350)
+
+def testBedMergeThickBounds(request):
+    """Test that thickStart/thickEnd are properly merged"""
+    bed1 = Bed("chr4", 100, 300, "bed1", strand="+", thickStart=120, thickEnd=200,
+               blocks=[BedBlock(100, 300)])
+    bed2 = Bed("chr4", 150, 400, "bed2", strand="+", thickStart=180, thickEnd=350,
+               blocks=[BedBlock(150, 400)])
+    mergedBed = bedMergeBlocks("merged", [bed1, bed2])
+    # Should take min of thickStart and max of thickEnd
+    assert mergedBed.thickStart == 120
+    assert mergedBed.thickEnd == 350
+
+def testBedMergeSingle(request):
+    """Test merging a single BED (should return a copy)"""
+    bed1 = Bed("chr5", 100, 300, "bed1", strand="+", blocks=[BedBlock(100, 150), BedBlock(200, 300)])
+    mergedBed = bedMergeBlocks("merged", [bed1])
+    assert mergedBed.chrom == "chr5"
+    assert mergedBed.chromStart == 100
+    assert mergedBed.chromEnd == 300
+    assert mergedBed.name == "merged"
+    assert len(mergedBed.blocks) == 2
+
+def testBedMergeMultipleComplex(request):
+    """Test merging multiple BEDs with complex overlapping patterns"""
+    bed1 = Bed("chr6", 100, 400, "bed1", strand="+",
+               blocks=[BedBlock(100, 150), BedBlock(200, 250), BedBlock(350, 400)])
+    bed2 = Bed("chr6", 120, 380, "bed2", strand="+",
+               blocks=[BedBlock(120, 180), BedBlock(240, 280), BedBlock(360, 380)])
+    bed3 = Bed("chr6", 130, 420, "bed3", strand="+",
+               blocks=[BedBlock(130, 160), BedBlock(275, 285), BedBlock(400, 420)])
+    mergedBed = bedMergeBlocks("merged", [bed1, bed2, bed3])
+    # Should merge overlapping/adjacent blocks
+    assert mergedBed.chromStart == 100
+    assert mergedBed.chromEnd == 420
+    assert len(mergedBed.blocks) == 3
+    assert mergedBed.blocks[0] == BedBlock(100, 180)
+    assert mergedBed.blocks[1] == BedBlock(200, 285)
+    assert mergedBed.blocks[2] == BedBlock(350, 420)
+
+def testBedMergeEmpty(request):
+    """Test that merging empty list raises exception"""
+    try:
+        bedMergeBlocks("merged", [])
+        assert False, "Expected BedException"
+    except BedException as e:
+        assert "can't merge list with no BEDs" in str(e)
+
+def testBedMergeDifferentChroms(request):
+    """Test that merging BEDs on different chromosomes raises exception"""
+    bed1 = Bed("chr1", 100, 200, "bed1", strand="+", blocks=[BedBlock(100, 200)])
+    bed2 = Bed("chr2", 100, 200, "bed2", strand="+", blocks=[BedBlock(100, 200)])
+    try:
+        bedMergeBlocks("merged", [bed1, bed2])
+        assert False, "Expected BedException"
+    except BedException as e:
+        assert "different chromosomes" in str(e)
+
+def testBedMergeDifferentStrands(request):
+    """Test that merging BEDs on different strands raises exception"""
+    bed1 = Bed("chr1", 100, 200, "bed1", strand="+", blocks=[BedBlock(100, 200)])
+    bed2 = Bed("chr1", 100, 200, "bed2", strand="-", blocks=[BedBlock(100, 200)])
+    try:
+        bedMergeBlocks("merged", [bed1, bed2])
+        assert False, "Expected BedException"
+    except BedException as e:
+        assert "different strands" in str(e)
+
+def testBedMergeDifferentNumStdCols(request):
+    """Test that merging BEDs with different numStdCols raises exception"""
+    bed1 = Bed("chr1", 100, 200, "bed1", strand="+", blocks=[BedBlock(100, 200)])
+    bed2 = Bed("chr1", 100, 200, "bed2", strand="+", numStdCols=6)
+    try:
+        bedMergeBlocks("merged", [bed1, bed2])
+        assert False, "Expected BedException"
+    except BedException as e:
+        assert "standard columns" in str(e)
 
 def testFixScores(request):
     with open(ts.get_test_output_file(request, ".bed"), 'w') as fh:
