f1e3649d6772a1b3d3685e0708a3bf18d0342845
Mark Diekhans <markd@ucsc.edu>
2026-05-23 13:03:55 -0700

    added autoSql float array parsers

--- lib/pycbio/hgdata/autoSql.py
+++ lib/pycbio/hgdata/autoSql.py
@@ -20,8 +20,8 @@ def strArraySplit(commaStr):
 def strArrayJoin(strs):
     """formatter for a list of values into a comma separated string, not-str values are
     converted to a string"""
-    if strs is None:
-        return ","
+    if (strs is None) or (len(strs) == 0):
+        return ""
     return ",".join([str(s) for s in strs]) + ","
 
 
@@ -41,14 +41,37 @@ def intArraySplit(commaStr):
 
 def intArrayJoin(ints):
     "formatter for a list of ints into a comma seperated string"
-    if ints is not None:
-        strs = []
-        for i in ints:
-            strs.append(str(i))
-        return ",".join(strs) + ","
-    else:
-        return ","
+    if (ints is None) or (len(ints) == 0):
+        return ""
+    strs = []
+    for i in ints:
+        strs.append(str(i))
+    return ",".join(strs) + ","
 
 
 # TSV typeMap tuple for str arrays
 intArrayType = (intArraySplit, intArrayJoin)
+
+##
+# float arrays
+##
+def floatArraySplit(commaStr):
+    "parser for comma-separated string list into a list of floats"
+    floats = []
+    for s in strArraySplit(commaStr):
+        floats.append(float(s))
+    return floats
+
+
+def floatArrayJoin(floats, fmt=None):
+    "formatter for a list of floats a comma seperated string"
+    if (floats is None) or (len(floats) == 0):
+        return ""
+    strs = []
+    for f in floats:
+        strs.append(fmt.format(f) if fmt is not None else str(f))
+    return ",".join(strs) + ","
+
+
+# TSV typeMap tuple for str arrays
+floatArrayType = (floatArraySplit, floatArrayJoin)
