9527e91673c2e27e93eb1989394b310411649de0
Mark Diekhans <markd@ucsc.edu>
2026-06-07 22:29:11 -0700

    initial implementation of trackdb building class

---
+++ lib/pycbio/hgbrowser/trackdb.py
@@ -0,0 +1,164 @@
+# Copyright 2006-2025 Mark Diekhans
+"""
+Create trackDb files for hubs.
+"""
+
+INDENT = "    "
+
+
+class Priority:
+    "auto-incrementing priority counter for ordering tracks"
+    def __init__(self, val=1):
+        self.val = val
+
+    def __call__(self):
+        return self.val
+
+    def __str__(self):
+        return str(self.val)
+
+    def incr(self, val=1):
+        self.val += val
+
+    def __iadd__(self, val):
+        self.val += val
+        return self
+
+
+def quote_setting(v):
+    "replace spaces so a value can be used in a space-delimited context"
+    return v.replace(' ', '_')
+
+
+class Track:
+    """A trackDb track stanza.  Settings come from the class DEFAULTS merged
+    with keyword arguments (kwargs win); those with a value of None are
+    dropped."""
+
+    DEFAULTS = {}
+
+    def __init__(self, track, shortLabel, longLabel=None, **settings):
+        self.track = track
+        self.shortLabel = shortLabel
+        self.longLabel = longLabel if longLabel is not None else shortLabel
+        self.settings = {k: v for k, v in (self.DEFAULTS | settings).items()
+                         if v is not None}
+
+    def __setitem__(self, key, val):
+        if val is None:
+            self.settings.pop(key, None)
+        else:
+            self.settings[key] = val
+
+    def __getitem__(self, key):
+        return self.settings[key]
+
+    def _own_lines(self):
+        lines = [
+            f"track {self.track}",
+            f"shortLabel {self.shortLabel}",
+            f"longLabel {self.longLabel}",
+        ]
+        for key, val in self.settings.items():
+            lines.append(f"{key} {val}")
+        return lines
+
+    def stanzas(self, level=0):
+        """Return this track (and any subtracks) as a list of stanza strings,
+        each indented for the given nesting level."""
+        prefix = INDENT * level
+        return ['\n'.join(prefix + line for line in self._own_lines())]
+
+    def __str__(self):
+        return '\n\n'.join(self.stanzas()) + '\n'
+
+
+class Container(Track):
+    """A track that contains subtracks (e.g. super or composite track)."""
+
+    def __init__(self, track, shortLabel, longLabel=None, **settings):
+        super().__init__(track, shortLabel, longLabel=longLabel, **settings)
+        self.subtracks = []
+
+    def add(self, subtrack):
+        "add a subtrack, defaulting its parent to this container; returns it"
+        subtrack.settings.setdefault("parent", self.track)
+        self.subtracks.append(subtrack)
+        return subtrack
+
+    def stanzas(self, level=0):
+        stanzas = super().stanzas(level)
+        for subtrack in self.subtracks:
+            stanzas.extend(subtrack.stanzas(level + 1))
+        return stanzas
+
+
+class DataTrack(Track):
+    "a track displaying data from a bigDataUrl"
+
+    DEFAULTS = {
+        "visibility": "hide",
+    }
+
+    def __init__(self, track, shortLabel, longLabel=None, *,
+                 trackType, bigDataUrl, **settings):
+        super().__init__(track, shortLabel, longLabel=longLabel,
+                         type=trackType, bigDataUrl=bigDataUrl, **settings)
+
+
+class PslTrack(DataTrack):
+    "a bigPsl alignment track with the usual indel/diff-base settings"
+
+    DEFAULTS = {
+        **DataTrack.DEFAULTS,
+        "searchIndex": "name",
+        "baseColorUseSequence": "lfExtra",
+        "indelDoubleInsert": "on",
+        "indelQueryInsert": "on",
+        "showDiffBasesAllScales": ".",
+        "showDiffBasesMaxZoom": 10000,
+    }
+
+    def __init__(self, track, shortLabel, longLabel=None, *,
+                 bigDataUrl, **settings):
+        super().__init__(track, shortLabel, longLabel=longLabel,
+                         trackType="bigPsl", bigDataUrl=bigDataUrl, **settings)
+
+
+class SuperTrack(Container):
+    "a super track grouping other tracks"
+
+    DEFAULTS = {
+        "superTrack": "on hide",
+    }
+
+
+class CompositeTrack(Container):
+    "a composite track grouping subtracks"
+
+    DEFAULTS = {
+        "compositeTrack": "on",
+        "visibility": "hide",
+    }
+
+
+class TrackDb:
+    "collection of top-level tracks making up a trackDb file"
+
+    def __init__(self, tracks=()):
+        self.tracks = list(tracks)
+
+    def add(self, track):
+        "add a top-level track; returns it"
+        self.tracks.append(track)
+        return track
+
+    def format(self):
+        "format all tracks into a complete trackDb string"
+        stanzas = []
+        for track in self.tracks:
+            stanzas.extend(track.stanzas())
+        return '\n\n'.join(stanzas) + '\n'
+
+    def __str__(self):
+        return self.format()
---
+++ tests/libtests/pycbio/hgbrowser/test_trackdb.py
@@ -0,0 +1,129 @@
+# Copyright 2006-2025 Mark Diekhans
+import sys
+if __name__ == '__main__':
+    sys.path.insert(0, "../../../../lib")
+from pycbio.hgbrowser.trackdb import (Priority, quote_setting, Track, Container,
+                                      DataTrack, PslTrack, SuperTrack,
+                                      CompositeTrack, TrackDb)
+
+
+def testPriority():
+    p = Priority()
+    assert p() == 1
+    assert str(p) == "1"
+    p.incr()
+    assert p() == 2
+    p.incr(3)
+    assert p() == 5
+    p += 5
+    assert p() == 10
+    assert isinstance(p, Priority)
+
+
+def testQuoteSetting():
+    assert quote_setting("two words") == "two_words"
+    assert quote_setting("none") == "none"
+
+
+def testTrackBasic():
+    trk = Track("foo", "Foo")
+    assert str(trk) == ("track foo\n"
+                        "shortLabel Foo\n"
+                        "longLabel Foo\n")
+
+
+def testTrackLongLabel():
+    trk = Track("foo", "Foo", "The Foo Track", color="1,2,3")
+    assert str(trk) == ("track foo\n"
+                        "shortLabel Foo\n"
+                        "longLabel The Foo Track\n"
+                        "color 1,2,3\n")
+
+
+def testTrackNoneDropped():
+    trk = Track("foo", "Foo", color=None, html="x.html")
+    assert "color" not in str(trk)
+    assert "html x.html" in str(trk)
+
+
+def testTrackItemAccess():
+    trk = Track("foo", "Foo")
+    trk["color"] = "1,2,3"
+    assert trk["color"] == "1,2,3"
+    trk["color"] = None          # None removes
+    assert "color" not in trk.settings
+
+
+def testDataTrack():
+    trk = DataTrack("foo", "Foo", trackType="bigBed", bigDataUrl="foo.bb")
+    s = str(trk)
+    assert "type bigBed" in s
+    assert "bigDataUrl foo.bb" in s
+    assert "visibility hide" in s
+
+
+def testPslTrackDefaults():
+    trk = PslTrack("aln", "Aln", bigDataUrl="aln.bb")
+    s = str(trk)
+    assert "type bigPsl" in s
+    assert "searchIndex name" in s
+    assert "showDiffBasesMaxZoom 10000" in s
+
+
+def testPslTrackOverride():
+    trk = PslTrack("aln", "Aln", bigDataUrl="aln.bb",
+                   visibility="full", searchIndex="name,desc")
+    s = str(trk)
+    assert "visibility full" in s
+    assert "searchIndex name,desc" in s
+
+
+def testSuperTrack():
+    trk = SuperTrack("grp", "Group")
+    assert "superTrack on hide" in str(trk)
+
+
+def testCompositeTrack():
+    trk = CompositeTrack("comp", "Comp", visibility="pack")
+    s = str(trk)
+    assert "compositeTrack on" in s
+    assert "visibility pack" in s
+
+
+def testContainerAddSetsParent():
+    comp = CompositeTrack("comp", "Comp")
+    sub = comp.add(DataTrack("foo", "Foo", trackType="bigBed", bigDataUrl="foo.bb"))
+    assert sub.settings["parent"] == "comp"
+
+
+def testContainerExplicitParentKept():
+    comp = CompositeTrack("comp", "Comp")
+    sub = comp.add(Track("foo", "Foo", parent="comp on"))
+    assert sub.settings["parent"] == "comp on"   # setdefault leaves it
+
+
+def testNestingIndentation():
+    sup = SuperTrack("grp", "Group")
+    comp = sup.add(CompositeTrack("comp", "Comp"))
+    comp.add(PslTrack("hg38", "hg38", bigDataUrl="hg38.bb"))
+    stanzas = sup.stanzas()
+    assert len(stanzas) == 3
+    assert stanzas[0].startswith("track grp")
+    assert stanzas[1].startswith("    track comp")          # level 1
+    assert stanzas[2].startswith("        track hg38")      # level 2
+
+
+def testTrackDb():
+    tdb = TrackDb()
+    tdb.add(Track("a", "A"))
+    tdb.add(Track("b", "B"))
+    out = tdb.format()
+    assert str(tdb) == out
+    assert out.endswith("\n")
+    assert "\n\n" in out                      # blank line between stanzas
+    assert out.count("track ") == 2
+
+
+def testTrackDbInitList():
+    tdb = TrackDb([Track("a", "A")])
+    assert "track a" in tdb.format()
